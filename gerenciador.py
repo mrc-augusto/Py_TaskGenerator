dfbbad59eeb82cc7cf966a3b97a7ef723fac2acc
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2025-06-11 16:58:59 -0300

    initial commit

---
+++ gerenciador.py
@@ -0,0 +1,15 @@
+while True:
+    print('\nMenu do Gerenciador de Tarefas: ')
+    print('1. Adicionar Tarefa ')
+    print('2. Ver Tarefas')
+    print('3. Atualizar Tarefa')
+    print('4. Completar Tarefa')
+    print('5. Deletar Tarefas Completadas')
+    print('6. Sair ')
+
+    escolha = input('Digite a sua escolha:')
+
+    if escolha == '6':
+        break
+
+    print('Programa Finalizado')
