ac87b26ad6b051a57650360e6ebde24562b821de
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2025-06-12 18:31:39 -0300

    add and see tasks functions created

--- gerenciador.py
+++ gerenciador.py
@@ -1,3 +1,24 @@
+def adicionar_tarefa(tarefas, nome_tarefa):
+    tarefa = {'tarefa': nome_tarefa, 'completa': False}
+    tarefas.append(tarefa)
+    print(f'Tarefa {nome_tarefa} foi adicionada com sucesso!') 
+    return
+
+def ver_tarefas(tarefas):
+    if not tarefas:
+        print('Nenhuma tarefa foi adicionada.')
+        return
+
+    elif tarefas:
+        print('\nLista de Tarefas: ')
+        for indice, tarefa, in enumerate(tarefas, start=1):
+            status = '✔' if tarefa['completa'] else 'Pendente '
+            nome_tarefa = tarefa['tarefa']
+            print(f'{indice}. [{status}] {nome_tarefa}')
+            
+1
+tarefas = []
+
 while True:
     print('\nMenu do Gerenciador de Tarefas: ')
     print('1. Adicionar Tarefa ')
@@ -9,7 +30,14 @@ while True:
 
     escolha = input('Digite a sua escolha:')
 
-    if escolha == '6':
-        break
+    if escolha == '1':
+        nome_tarefa = input('Digite o nome da tarefa:')
+        adicionar_tarefa(tarefas, nome_tarefa)
 
-    print('Programa Finalizado')
+    elif escolha == '2':
+        ver_tarefas(tarefas)
+
+    elif escolha == '6':
+        break
+    
+print('Programa Finalizado')
